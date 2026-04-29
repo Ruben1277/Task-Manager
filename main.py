0f4550be1aad31f172103d151876fdb24838919b
Gabriel Ruben Félix Lima <gbl132006@gmail.com>
2026-04-28 21:25:18 -0300

    feet: add user duplicate validation, register of user, saving system

--- main.py
+++ main.py
@@ -15,30 +15,29 @@ def load_data():
     except (FileNotFoundError , json.JSONDecodeError):
         return data_model
 def save_data(data):
-    try: 
-        with open("data.json", "w") as file:
-            return json.dump(data , file)
-    except:
-        print("wasn't possible saving your data, try again")
+  with open("data.json", "w") as file:
+    json.dump(data , file)
+
 def register(data, user_name, password):
     users = data['users']
     _user_name = user_name.lower()
-    _password = password.lower()
-    if _user_name in users.keys():
-        if users[_user_name]["password"] == password:
-            print(users[_user_name])
-        else:
-            print("password invalid try again")
+    _password = password.strip()
+    if not user_name or not _password :
+        print("Invalid input ")
+        return
+    if _user_name in users:
+       print("User already exists")
+       print(f"{_user_name} {users[_user_name]}")
+       return
     else:
-        print("new login")
+   
         users[_user_name] = {"password": _password , "tasks" : []}
-        data['users'] = users
-        print(f"{_user_name} {users[_user_name]}")
+        print(f" user created:\n {_user_name} {users[_user_name]}")
         save_data(data)
 
 def main():
     data = load_data()
-    register(data , "gamer" , "jon11")
+    register(data , "" , "")
 
 if __name__ == "__main__":
     main()

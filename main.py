1b67afffd0cb9f83277d41f3a65a1d83ebaac5c8
Gabriel Ruben Félix Lima <gbl132006@gmail.com>
2026-05-02 12:42:43 -0300

    I correct structure, return but need correction yet, clear OS fixed, flux is better

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import os
 import hashlib 
-#note: add tomorrow : add login feature with validation and password hashing for security
+
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 data_model = {
@@ -31,85 +31,100 @@ def save_data(data):
 def login(data , user_name, password):
     users = data['users']
     _user_name = user_name.lower().strip()
-    #validation on position wrong, solve tomorrow
-    _password = hash_password(password.strip())
-    if not _user_name or not _password :
+
+    if not _user_name or not password.strip() :
         print("Invalid input ")
-        return False
+        return False , None
+    _password = hash_password(password.strip())
+
     if _user_name in users:
         if _password == users[_user_name]['password']:
             print(f"Login sucessful")
-            return True
+            return True , _user_name
         else:
             print('Incorrect password, try again')
-            return False
+            return False, None
         
     else:
         print("User don't exist, try other user")
-        return False
+        return False , None
 
     #register with validation
 def register(data, user_name, password):
     
     users = data['users']
     _user_name = user_name.lower().strip()
-    #validation on position wrong, solve tomorrow
-    _password = hash_password(password.strip())
-    if not _user_name or not _password :
+    if not _user_name or not password.strip() :
         print("Invalid input ")
-        return False
+        return False , None
+
+    _password = hash_password(password.strip())
+
     if _user_name in users:
        print("User already exists")
-       return False
+       return False , None
     else:
-   
         users[_user_name] = {"password": _password , "tasks" : []}
         save_data(data)
-        return True
+        return True , _user_name
 def cancel():
     pass
 def input_control(input_type, data):
     match input_type :
-        case 'login':
+        case '1':
             username = input(menu('login_username'))
             password = input(menu('login_password'))
-            login(data, username, password)
-        case 'register':
+            sucess, current_user = login(data, username, password)
+            if sucess:
+                return current_user
+            else:
+                print( 'Not possible login , try again later: ')
+        case '2':
             username = input(menu('register_username'))
             password = input(menu('register_password'))    
-            register(data , username, password)        
+            sucess , current_user = register(data , username, password)    
+            if sucess:
+                return current_user
+            else:
+                print('Not possible register , try again later')
+                return False 
         case _:
             print('Write a valid function: ')
+            return False
 def clear_terminal():
-    #this is limited to windows, solce tomorrow for other OS
-    os.system("cls")
+
+    os.system("cls" if os.name == 'nt' else 'clear')
 
 def menu(menu_mode):
     match menu_mode:
         case 'main':
-            #mixed with clear terminal, solve tomorrow
-            clear_terminal()
-            return('Feats: |1| login , |2| register , |3| delete account , |4| show account \n')
+            return('Feats: |1| login , |2| register , |3| delete account , |4| show account , |5| exit\n')
         case 'login_username':
-                clear_terminal()
                 return "Write your username: "
         case 'login_password':
-                clear_terminal()
                 return "Write your password: "  
         case 'register_username':
-            clear_terminal()
             return "Make your username: "
         case 'register_password':
-            clear_terminal()
             return "Make your password: "
                  
 
 
 def main():
-    print(menu("main"))
+    clear_terminal()
     data = load_data()
-    #solve the flux of system and add a loop to control the system
-    input_control('register', data)
+    current_user = None
+    while True:
+        print(menu('main'))
+        choice_function = input('Which fuction your want to execute: ')
+        if choice_function == '5':
+            break
+        _current_user = input_control(choice_function, data)
+        if _current_user:
+            current_user = _current_user
+        print(f"Current User : {current_user}")
+        input('Press Enter to continue: ')
+        clear_terminal()
     
 if __name__ == "__main__":
     main()
